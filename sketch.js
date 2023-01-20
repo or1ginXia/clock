a2ee8fa952d48e025e816755178f804f12945b50
or1ginXia <81893736+or1ginXia@users.noreply.github.com>
2023-01-20 00:04:52 -0500

    version2

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 function setup() {
 	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	colorMode(RGB, 255, 255, 255, 1);//color mode
 	noFill();
 }
 
@@ -7,32 +8,51 @@ function draw() {
 	// create background
 	background(225);
 
-	//maping value
-	var hour_s = map(hour(), 0, 24, 0, 2*PI); 
-  	var minute_s = map(minute(), 0, 60, 0, 2*PI);
-  	var second_s = map(second(), 0, 60, 0, 2*PI);
+	// time
+	var hour_s = map(hour(), 0, 24, -PI/2, PI/2); 
+  	var minute_s = map(minute(), 0, 60, -PI, 0);
+  	var second_s = map(second(), 0, 60, 50, 200);
 
 	// change the origin point
 	translate(width/2, height/2);
+
+	// scale
+	strokeWeight(7);
+	if(hour()<12){
+		// morning
+		stroke(0, 200, 255);
+		line(25, 0, 100, 0);
+	}else{
+		// afternoon
+		stroke(100, 0, 255);
+		line(25, 0, 100, 0);
+	}
 	
-	push();
-	stroke(255,50,0)
-	strokeWeight(39);
-	rotate(hour_s);
-	arc(0, 0, 350, 350, -PI/2 + 2*PI/45 , -PI/2 + 2*PI/45*44);
-	pop();
+	// second
+	stroke(255, 0, 0);
+	strokeWeight(30)
+	// arc(0, 0, second_s, second_s, 5/4*PI, 1.5*PI);
+	for(i = 0; i < 30; i++){
+		strokeWeight(i);
+		arc(0, 0, second_s, second_s, i*1.5*PI/30, (i+1)*1.5*PI/30);
+	}
 
-	push();
-	stroke(255,150,0)
-	strokeWeight(30);
-	rotate(minute_s);
-	arc(0, 0, 250, 250, -PI/2 + 2*PI/40 , -PI/2 + 2*PI/40*39);
-	pop();
+	// miniute
+	strokeWeight(40);
+	stroke(255, 90, 0, 0.4);
+	arc(0, 0, 300, 300, -PI, 0);
+	
+	strokeWeight(40);
+	stroke(255, 90, 0, 1);
+	arc(0, 0, 300, 300, -PI, minute_s);
 
-	push();
-	stroke(255,200,0)
-	strokeWeight(20);
-	rotate(second_s);
-	arc(0, 0, 160, 160, -PI/2 + 2*PI/35 , -PI/2 + 2*PI/35*34);
-	pop();
+	// hour
+	strokeWeight(50);
+	stroke(255, 150, 0, 0.4);
+	arc(0, 0, 430, 430, -PI/2, PI/2);
+
+	strokeWeight(50);
+	stroke(255, 150, 0, 1);
+	arc(0, 0, 430, 430, -PI/2, hour_s);
+
 }
