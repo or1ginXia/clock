dcc6b6007aea14c925a78591f0a9591ba87b484b
or1ginXia <81893736+or1ginXia@users.noreply.github.com>
2023-01-20 01:09:39 -0500

    version3

--- sketch.js
+++ sketch.js
@@ -11,48 +11,38 @@ function draw() {
 	// time
 	var hour_s = map(hour(), 0, 24, -PI/2, PI/2); 
   	var minute_s = map(minute(), 0, 60, -PI, 0);
-  	var second_s = map(second(), 0, 60, 50, 200);
+  	var second_s = map(second(), 0, 60, 100, 200);
 
 	// change the origin point
 	translate(width/2, height/2);
 
 	// scale
-	strokeWeight(7);
-	if(hour()<12){
-		// morning
-		stroke(0, 200, 255);
-		line(25, 0, 100, 0);
-	}else{
-		// afternoon
-		stroke(100, 0, 255);
-		line(25, 0, 100, 0);
-	}
+	strokeWeight(5);
+	stroke(200, 100, 90);
+	line(50, 0, 100, 0);
 	
 	// second
 	stroke(255, 0, 0);
-	strokeWeight(30)
-	// arc(0, 0, second_s, second_s, 5/4*PI, 1.5*PI);
-	for(i = 0; i < 30; i++){
-		strokeWeight(i);
-		arc(0, 0, second_s, second_s, i*1.5*PI/30, (i+1)*1.5*PI/30);
+	for(i = 0; i < 40; i++){
+		strokeWeight((20*i)**(1/2));
+		arc(0, 0, second_s, second_s, i*1.5*PI/40, (i+1)*1.5*PI/40);
 	}
 
 	// miniute
-	strokeWeight(40);
+	strokeWeight(38);
 	stroke(255, 90, 0, 0.4);
-	arc(0, 0, 300, 300, -PI, 0);
+	arc(0, 0, 315, 315, -PI, 0);
 	
-	strokeWeight(40);
+	strokeWeight(38);
 	stroke(255, 90, 0, 1);
-	arc(0, 0, 300, 300, -PI, minute_s);
+	arc(0, 0, 315, 315, -PI, minute_s);
 
 	// hour
 	strokeWeight(50);
 	stroke(255, 150, 0, 0.4);
-	arc(0, 0, 430, 430, -PI/2, PI/2);
+	arc(0, 0, 445, 445, -PI/2, PI/2);
 
 	strokeWeight(50);
 	stroke(255, 150, 0, 1);
-	arc(0, 0, 430, 430, -PI/2, hour_s);
-
+	arc(0, 0, 445, 445, -PI/2, hour_s);
 }
